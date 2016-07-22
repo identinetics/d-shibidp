d68e16ff2a0cd0ed28593a5c4f0668472cf6e986
r2h2 <rainer@hoerbe.at>
2016-07-22 20:33:48 +0200

    test symlings with -e; add -f to ln -s

--- build_prepare.sh
+++ build_prepare.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 
 # download and verify components to be installed with docker build
 mkdir -p install/downloads
@@ -7,23 +6,23 @@ cd install/downloads
 # JAVA JRE 1.8.0 Update 102
 JRE_DOWNLOAD_URL='http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.tar.gz'
 JRE_CHECKSUM='214ff6b52f5b1bccfc139dca910cea25f6fa19b9b96b4e3c10e699cd3e780dfb  jre-linux-x64.tar.gz'
-if [ ! -d "jre1.8.0" ]; then
+if [ ! -e "jre1.8.0" ]; then
     rm -rf jre1.8.0_*
     wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" \
         -O jre-linux-x64.tar.gz $JRE_DOWNLOAD_URL
     echo $JRE_CHECKSUM | sha256sum -c -
     tar -xzf jre-linux-x64.tar.gz
     rm jre-linux-x64.tar.gz
-    ln -s jre1.8.0_* jre1.8.0
+    ln -sf jre1.8.0_* jre1.8.0
 fi
-if [ ! -d "jetty" ]; then
+if [ ! -e "jetty" ]; then
     # download jetty int ./jetty. Then jetty9-dta-ssl-1.0.0.jar, logback, slf4j go into jetty-base
     jetty_version='9.3.3.v20150827'
     wget -O jetty.zip \
         "https://eclipse.org/downloads/download.php?file=/jetty/$jetty_version/dist/jetty-distribution-$jetty_version.zip&r=1"
     echo "f281590d3fcde0f9a8fce0691aa1a1a838d3666a49474f9c8bc01a5468ef50818358233bcb958b2cb434b097c9d0ccda626616fb4731c5754b13f0740f76f1f5  jetty.zip" | sha512sum -c -
     unzip -q jetty.zip
-    ln -s jetty-distribution-$jetty_version jetty
+    ln -sf jetty-distribution-$jetty_version jetty
     rm jetty.zip
 
     wget https://build.shibboleth.net/nexus/content/repositories/releases/net/shibboleth/utilities/jetty9/jetty9-dta-ssl/1.0.0/jetty9-dta-ssl-1.0.0.jar
@@ -37,7 +36,7 @@ if [ ! -d "jetty" ]; then
     rm logback.zip
     mkdir -p ../jetty-base/lib/logging
     cd logback
-        ls | grep -v .jar | xargs rm -rf
+        find . -name '*.jar' -delete
         cp -p logback-classic-1.1.6.jar logback-core-1.1.6.jar logback-access-1.1.6.jar ../../jetty-base/lib/logging
     cd ..
 
@@ -45,17 +44,16 @@ if [ ! -d "jetty" ]; then
     unzip slf4j.zip
     rm slf4j.zip
     cd slf4j-1.7.18
-        ls | grep -v slf4j-api-1.7.18.jar | xargs rm -rf
         cp -p slf4j-api-1.7.18.jar ../../jetty-base/lib/logging/
     cd ..
 fi
-if [ ! -e "shibboleth-idp-distribution" ]; then
-    wget -O shibboleth-identity-provider.zip \
-        "https://shibboleth.net/downloads/identity-provider/3.2.1/shibboleth-identity-provider-3.2.1.zip"
-    echo "5b4e10afb5af2bd02fb16261c34fef8da5b47dabd501de101f00e88529eec2cf25b3518c4d865027820e66ab52730310b01dff7f223cb53194b48fe89ade0954  shibboleth-identity-provider.zip" | sha512sum -c -
-    unzip -q shibboleth-identity-provider.zip
-    rm shibboleth-identity-provider.zip
-    ln -s shibboleth-identity-provider-3.2.1 shibboleth-idp-distribution
-fi
+ if [ ! -e "shibboleth-idp-distribution" ]; then
+     wget -O shibboleth-identity-provider.zip \
+         "https://shibboleth.net/downloads/identity-provider/3.2.1/shibboleth-identity-provider-3.2.1.zip"
+     echo "5b4e10afb5af2bd02fb16261c34fef8da5b47dabd501de101f00e88529eec2cf25b3518c4d865027820e66ab52730310b01dff7f223cb53194b48fe89ade0954  shibboleth-identity-provider.zip" | sha512sum -c -
+     unzip -q shibboleth-identity-provider.zip
+     rm shibboleth-identity-provider.zip
+     ln -sf shibboleth-identity-provider-3.2.1 shibboleth-idp-distribution
+ fi
 
 cd ../..
