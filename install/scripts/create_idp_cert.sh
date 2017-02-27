39d1efcfc6c8faab8678a0902c76f9d11ebd0cb0
r2h2 <rainer@hoerbe.at>
2017-02-27 21:32:42 +0100

    add create_idp_cert.sh

---
+++ install/scripts/create_idp_cert.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Create an X.509 cert to be used for signing and/or encrpytion
+
+cd /opt/shibboleth-idp/credentials
+
+sudo java -cp "../webapp/WEB-INF/lib/*:../bin/lib/*" \
+  net.shibboleth.utilities.java.support.security.SelfSignedCertificateGenerator \
+  --lifetime 5 \
+  --certfile idp.crt.new \
+  --keyfile idp.key.new \
+  --hostname $IDP_FQDN \
+  --uriAltName $ENTITYID
+
