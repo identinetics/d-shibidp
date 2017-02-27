fbc7c6d13a42ef7361426315b850edcc56c1bdb7
r2h2 <rainer@hoerbe.at>
2017-02-27 19:52:59 +0100

    chmod +x install/scripts/*.sh; set -e -o pipefail

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ "$KEYSTOREPW" == 'changeit' ]; then
+    echo "Set KEYSTOREPW to a secret value before starting the IDP" && exit 1
+fi
+
 export JAVA_HOME=/opt/jre1.8.0
 export JETTY_HOME=/opt/jetty/
 export JETTY_BASE=/opt/jetty-base/
